6456efd7428ce0e199bceb03e20dff6918707fcf
MarijnSt <marijn.stammeleer@gmail.com>
2019-08-28 16:59:52 +0200

    testing step 5

--- backend/utils/list_helper.js
+++ backend/utils/list_helper.js
@@ -18,8 +18,82 @@ const favoriteBlog = (blogs) => {
     return mostLikes
 }
 
+const mostBlogs = (blogs) => {
+    const array = blogs
+    const namesArray = []
+    const numbersArray = []
+    let indexMost
+
+    // fill array with unique authors
+    const findAuthors = () => {
+        array.forEach(element => {
+            if (!namesArray.includes(element.author)){
+                namesArray.push(element.author)
+            }
+        });
+    }
+
+    // use set to look for number of blogs and find biggest number for author index
+    const findBlogs = () => {
+        namesArray.forEach(element => {
+            const numberOfBlogs = array.filter(el => el.author.includes(element)).length
+            numbersArray.push(numberOfBlogs)
+        })
+        indexMost = numbersArray.indexOf(Math.max(...numbersArray))
+    }
+
+    findAuthors()
+    findBlogs()
+
+    const result = {
+        Author: namesArray[indexMost],
+        Blogs: numbersArray[indexMost]
+    }
+
+    return result
+}
+
+const mostLikes = (blogs) => {
+    const array = blogs
+    const namesArray = []
+    const numbersArray = []
+    let indexMost
+
+    // fill array with unique authors
+    const findAuthors = () => {
+        array.forEach(element => {
+            if (!namesArray.includes(element.author)){
+                namesArray.push(element.author)
+            }
+        });
+    }
+
+    // use set to look for number of blogs and find biggest number for author index
+    const findBlogs = () => {
+        namesArray.forEach(element => {
+            const allBlogs = array.filter(el => el.author.includes(element))
+            const reducer = (acc, curr) => acc + curr.likes
+            const total = allBlogs.reduce(reducer, 0)
+            numbersArray.push(total)
+        })
+        indexMost = numbersArray.indexOf(Math.max(...numbersArray))
+    }
+
+    findAuthors()
+    findBlogs()
+
+    const result = {
+        Author: namesArray[indexMost],
+        Likes: numbersArray[indexMost]
+    }
+
+    return result
+}
+
 module.exports = {
     dummy,
     totalLikes,
-    favoriteBlog
+    favoriteBlog,
+    mostBlogs,
+    mostLikes
 }
